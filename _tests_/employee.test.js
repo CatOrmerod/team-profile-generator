d0608eb77fe689e430a1c09e8f703652b021ce80
Cat Ormerod <catormerod@gmail.com>
2021-05-11 09:38:09 +1000

    Employee tests complete

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,20 +1,53 @@
 const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
-const employee = new Employee('Cat', '1', 'cat@gmail.com', 'Manager');
+//const employee = new Employee('Cat', '1', 'cat@gmail.com', 'Manager');
 
-test ('Can instantiate Employee instance', () => {
-    expect(employee.name).toBe('Cat');
-    expect(employee.id).toBe('1');
-    expect(employee.email).toBe('cat@gmail.com');
-    expect(employee.role).toBe('Manager');
-});
+describe('Employee', () => {
+    it('should instantiate Employee instance', () => {
+        let employee = new Employee('Cat', '1', 'cat@gmail.com', 'Manager');
+        expect(employee.name).toBe('Cat');
+        expect(employee.id).toBe('1');
+        expect(employee.email).toBe('cat@gmail.com');
+        expect(employee.role).toBe('Manager');
+    })
+    it('should set name via Constructor argument', () => {
+        let name = 'Cat';
+        let employee = new Employee(name);
+        expect(employee.name).toBe(name)
+    })
+    it('should set id via Constructor argument', () => {
+        let id = '1';
+        let employee = new Employee('Cat', id);
+        expect(employee.id).toBe(id)
+    })
+    it('should set email via Constructor argument', () => {
+        let email = 'Cat@gmail.com';
+        let employee = new Employee('Cat', '1', email);
+        expect(employee.email).toBe(email)
+    })
+    it('should get name via getName()', () => {
+        let name = 'Cat';
+        let employee = new Employee(name);
+        expect(employee.getName()).toBe(name)
+    })
+    it('should get id via getId()', () => {
+        let id = '1';
+        let employee = new Employee('Cat', id);
+        expect(employee.getId()).toBe(id)
+    })
+    it('should get email via getEmail()', () => {
+        let email = 'Cat@gmail.com';
+        let employee = new Employee('Cat', '1', email);
+        expect(employee.getEmail()).toBe(email)
+    })
+    it('should get role via getRole()', () => {
+        let role = 'Manager';
+        let employee = new Employee('Cat', '1', 'Cat@gmail.com', role);
+        expect(employee.getRole()).toBe(role)
+    })
+})
 
 
-// test ('Can set name via Constructor argument')
-
-// test ('Can set id via Constructor argument')
-
-// test ('Can set email via Constructor argument')
 
 // test ('Can get name via getName()')
 
